2c001160ece3a24c52493650f04fffc91cfbeecb
Salem Karani <salemkarani@gmail.com>
2014-11-03 20:12:49 -0600

    fourth

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
 
   def test_t2(self):
     r1 = Experiment.largest([3,2,1]) 
-    self.assertEqual(r1, 3)
+    self.assertEqual(r1, )
 
   def test_t3(self):
     r1 = Experiment.largest([3,2,3]) 
@@ -21,7 +21,7 @@ class MyTestCase(unittest.TestCase):
 
   def test_t5(self):
     r1 = Experiment.largest([3,-2,3,-1,4]) 
-    self.assertEqual(r1, 4)
+    self.assertEqual(r1, -3)
 
   def test_t6(self):
     r1 = Experiment.largest([-3,-2,-3,-1,-4]) 
