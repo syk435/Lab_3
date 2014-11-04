6f111d9ab6eb2a3f5aa4f26d267268219f034961
Salem Karani <salemkarani@gmail.com>
2014-11-03 20:10:57 -0600

    third

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -25,14 +25,14 @@ class MyTestCase(unittest.TestCase):
 
   def test_t6(self):
     r1 = Experiment.largest([-3,-2,-3,-1,-4]) 
-    self.assertEqual(r1, -1)
+    self.assertEqual(r1, -4)
 
   def test_t7(self):
     self.assertRaises(ValueError, Experiment.largest, [])
 	
   def test_logout(self):
     r1 = Experiment.largest([-1,-1,-1,-1,-4]) 
-    self.assertEqual(r1, -1)
+    self.assertEqual(r1, -9)
 
 if __name__ == '__main__':
   unittest.main()
