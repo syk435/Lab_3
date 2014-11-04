7b2672aaa8a51a3fa3031f3a514cecffb624b64c
Salem Karani <salemkarani@gmail.com>
2014-11-03 20:16:01 -0600

    fifth

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -11,22 +11,6 @@ class MyTestCase(unittest.TestCase):
     r1 = Experiment.largest([3,2,1]) 
     self.assertEqual(r1, )
 
-  def test_t3(self):
-    r1 = Experiment.largest([3,2,3]) 
-    self.assertEqual(r1, 3)
-
-  def test_t4(self):
-    r1 = Experiment.largest([3,2,3,4]) 
-    self.assertEqual(r1, 4)
-
-  def test_t5(self):
-    r1 = Experiment.largest([3,-2,3,-1,4]) 
-    self.assertEqual(r1, -3)
-
-  def test_t6(self):
-    r1 = Experiment.largest([-3,-2,-3,-1,-4]) 
-    self.assertEqual(r1, -4)
-
   def test_t7(self):
     self.assertRaises(ValueError, Experiment.largest, [])
 	
